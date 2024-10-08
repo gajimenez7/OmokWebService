6f9bdbfc1917127cc5628b9f25966d5f59ae982d
godi-png <eraticsyndicate@gmail.com>
2024-10-07 23:01:42 -0600

    finished helper funcitons for SmartStrategy

--- src/play/Player.php
+++ src/play/Player.php
@@ -14,173 +14,184 @@ include "GroupingPlayer.php";
 
 class Player extends SplPriorityQueue
 {
-    public const BROW = "beg";
-    public const EROW = "end";
+  public const BROW = "beg";
+  public const EROW = "end";
 
-    // parse queue/heap and check for addition of groupings
-    // return a boolean if it is in grouping
+  // parse queue/heap and check for addition of groupings
+  // return a boolean if it is in grouping
+  //
+  // check the "ends" first, to check if it is in a current
+  // grouping
+  //
+  // if the group is in a grouping with the middle places
+  // create a new grouping with
+  //
+  // create a clone of the player object to use as reference and traverse
+  public function inGrouping($group, $grouping, $theQueue)
+  {
+    $isInGrouping = false;
+    $whichEnd = "null";
+    $newGrouping = new GroupingPlayer();
+
+    // create new copy of the current queue
+    // $myQueue = new Player();
+
+    $myQueue = clone $theQueue;
+
+    $myQueue->setExtractFlags(1);
+
+    // get coordinate values from group we want
+    // to insert
+    $currX = $group[0];
+    $currY = end($group);
+
+    echo "x: " . $currX . " y: " . $currY . "\n";
+
+    // while the queue isn't empty, get values of first node
+    // parse through each x and y and compare with ours
+    // both x and y flags must be true for the group we want
+    // to add to be in the grouping
     //
-    // check the "ends" first, to check if it is in a current
-    // grouping
+    // if not, we will instantiate a new grouping object
+    // and insert group into new grouping
     //
-    // if the group is in a grouping with the middle places
-    // create a new grouping with
-    //
-    // create a clone of the player object to use as reference and traverse
-    public function inGrouping($group, $grouping, $theQueue)
-    {
-        $isInGrouping = false;
-        $whichEnd = "null";
+    // TODO: need to check if there can be a new group created
 
-        // create new copy of the current queue
-        // $myQueue = new Player();
+    // print_r($thisQueue);
 
-        $myQueue = clone $theQueue;
+    while (!$myQueue->isEmpty()) {
+      $arrOrientations = ["vertical", "horizontal", "forw_diagonal", "back_diagonal"];
+      // need to check orientation if there is one associated
+      $orient = $grouping->getOrientation();
 
-        $myQueue->setExtractFlags(1);
+      $arr = $myQueue->extract();
+      // check first and last coordinates
+      $firstX = $arr[0][0];
+      $firstY = $arr[0][1];
 
-        // get coordinate values from group we want
-        // to insert
-        $currX = $group[0];
-        $currY = end($group);
+      $lastX = end($arr)[0];
+      $lastY = end($arr)[1];
 
-        echo "x: " . $currX. " y: " . $currY . "\n";
+      // compare with first coords: x
+      echo "comparing first coords: " . $firstX . ", " . $firstY . "\n";
+      // first coordinate, can only check backwards
 
-        // while the queue isn't empty, get values of first node
-        // parse through each x and y and compare with ours
-        // both x and y flags must be true for the group we want
-        // to add to be in the grouping
-        //
-        // if not, we will instantiate a new grouping object
-        // and insert group into new grouping
-        //
-        // TODO: need to check if there can be a new group created
+      if (($orient == $arrOrientations[0]) && ($currX == $firstX && $currY == $firstY - 1)) {
+        echo "f1 \n";
+        $isInGrouping = true;
+        $whichEnd = self::BROW;
+      } elseif (($orient == $arrOrientations[1]) && ($currX == $firstX - 1 && $currY == $firstY)) {
+        echo "f2 \n";
+        $isInGrouping = true;
+        $whichEnd = self::BROW;
+      } elseif (($orient == $arrOrientations[2]) && ($currX == $firstX - 1 && $currY == $firstY - 1)) {
+        echo "f3 \n";
+        $isInGrouping = true;
+        $whichEnd = self::BROW;
+      } elseif (($orient == $arrOrientations[3]) && ($currX == $firstX - 1 && $currY == $firstY + 1)) {
+        echo "f4 \n";
+        $isInGrouping = true;
+        $whichEnd = self::BROW;
+      }
+      // compare with last coords: x
+      echo "comparing last coords: " . $lastX . ", " . $lastY . "\n";
+      if (($orient == $arrOrientations[0]) && ($currX == $lastX && $currY == $lastY + 1)) {
+        $isInGrouping = true;
+        echo "l1 \n";
+        $whichEnd = self::EROW;
+      } elseif (($orient == $arrOrientations[1]) && ($currX == $lastX + 1 && $currY == $lastY)) {
+        echo "l2 \n";
+        $isInGrouping = true;
+        $whichEnd = self::EROW;
+      } elseif (($orient == $arrOrientations[2]) && ($currX == $lastX + 1 && $currY == $lastY + 1)) {
+        echo "l3 \n";
+        $isInGrouping = true;
+        $whichEnd = self::EROW;
+      } elseif (($orient == $arrOrientations[3]) && ($currX == $lastX - 1 && $currY == $lastY - 1)) {
+        echo "l4 \n";
+        $isInGrouping = true;
+        $whichEnd = self::EROW;
+      }
 
-        // print_r($thisQueue);
+      // if it is in grouping just insert into grouping given in parameter
+      //
+      echo "in grouping boolean value: " . $isInGrouping . "\n";
 
-        while (!$myQueue->isEmpty()) {
-
-            $arrOrientations = ["vertical", "horizontal", "forw_diagonal", "back_diagonal"];
-            // need to check orientation if there is one associated
-            $orient = $grouping->getOrientation();
-
-            $arr = $myQueue->extract();
-            // check first and last coordinates
-            $firstX = $arr[0][0];
-            $firstY = $arr[0][1];
-
-            $lastX = end($arr)[0];
-            $lastY = end($arr)[1];
-
-            // compare with first coords: x
-            echo "comparing first coords: " . $firstX . ", " . $firstY . "\n";
-            // first coordinate, can only check backwards
-
-            if (($orient == $arrOrientations[0]) && ($currX == $firstX && $currY == $firstY - 1)) {
-                echo "f1 \n";
-                $isInGrouping = true;
-                $whichEnd = self::BROW;
-            } elseif (($orient == $arrOrientations[1]) && ($currX == $firstX - 1 && $currY == $firstY)) {
-                echo "f2 \n";
-                $isInGrouping = true;
-                $whichEnd = self::BROW;
-            } elseif (($orient == $arrOrientations[2]) && ($currX == $firstX - 1 && $currY == $firstY - 1)) {
-                echo "f3 \n";
-                $isInGrouping = true;
-                $whichEnd = self::BROW;
-            } elseif (($orient == $arrOrientations[3]) && ($currX == $firstX - 1 && $currY == $firstY + 1)) {
-                echo "f4 \n";
-                $isInGrouping = true;
-                $whichEnd = self::BROW;
-            }
-            // compare with last coords: x
-            echo "comparing last coords: " . $lastX . ", " . $lastY . "\n";
-            if (($orient == $arrOrientations[0]) && ($currX == $lastX && $currY == $lastY + 1)) {
-                $isInGrouping = true;
-                echo "l1 \n";
-                $whichEnd = self::EROW;
-            } elseif (($orient == $arrOrientations[1]) && ($currX == $lastX + 1 && $currY == $lastY)) {
-                echo "l2 \n";
-                $isInGrouping = true;
-                $whichEnd = self::EROW;
-            } elseif (($orient == $arrOrientations[2]) && ($currX == $lastX + 1 && $currY == $lastY + 1)) {
-                echo "l3 \n";
-                $isInGrouping = true;
-                $whichEnd = self::EROW;
-            } elseif (($orient == $arrOrientations[3]) && ($currX == $lastX - 1 && $currY == $lastY - 1)) {
-                echo "l4 \n";
-                $isInGrouping = true;
-                $whichEnd = self::EROW;
-            }
-
-            // if it is in grouping just insert into grouping given in parameter
-            //
-            echo "in grouping boolean value: " . $isInGrouping . "\n";
-
-            if ($isInGrouping == 1) {
-                echo "enter if and added group to grouping \n";
-                $grouping->addGroup($group);
-            } elseif ($isInGrouping != 1) {
-                echo "in else not grouping \n";
-                // else create a new grouping object and add it
-                for ($i = 0; $i < sizeOf($arr); $i++) {
-                    if ((($currX == $arr[$i][0]) && ($currY == $arr[$i][1] + 1)) ||
-                      (($currX == $arr[$i][0]) && ($currY == $arr[$i][1] - 1))) {
-                        $newGrouping = new GroupingPlayer();
-                        $newGrouping->addGroup($group);
-                        self::insert($newGrouping->getGrouping(), $newGrouping->getGroupSize());
-                    } elseif ((($currX == $arr[$i][0] - 1) && ($currY == $arr[$i][1])) ||
-                      (($currX == $arr[$i][0] + 1) && ($currY == $arr[$i][1]))) {
-                        $newGrouping = new GroupingPlayer();
-                        $newGrouping->addGroup($group);
-                        self::insert($newGrouping->getGrouping(), $newGrouping->getGroupSize());
-                    }
-                }
-                echo "created new group \n";
-            }
+      if ($isInGrouping == 1) {
+        echo "enter if and added group to grouping \n";
+        $grouping->addGroup($group);
+      } elseif ($isInGrouping != 1) {
+        echo "in else not grouping \n";
+        // else create a new grouping object and add it
+        for ($i = 0; $i < sizeOf($arr); $i++) {
+          if (($currX == $arr[$i][0]) && ($currY == $arr[$i][1] + 1)) {
+            $newGrouping->addGroup($group);
+            $newGrouping->addGroup([$arr[$i][0], $arr[$i][1]]);
+            echo "----in second if condition \n";
+            self::insert($newGrouping->getGrouping(), $newGrouping->getGroupSize());
+          } else if (($currX == $arr[$i][0]) && ($currY == $arr[$i][1] - 1)) {
+            $newGrouping->addGroup($group);
+            $newGrouping->addGroup([$arr[$i][0], $arr[$i][1]]);
+            echo "----in second if condition \n";
+            self::insert($newGrouping->getGrouping(), $newGrouping->getGroupSize());
+          } elseif (($currX == $arr[$i][0] - 1) && ($currY == $arr[$i][1])) {
+            $newGrouping->addGroup($group);
+            $newGrouping->addGroup([$arr[$i][0], $arr[$i][1]]);
+            echo "----in second else if condition \n";
+            self::insert($newGrouping->getGrouping(), $newGrouping->getGroupSize());
+          } else if (($currX == $arr[$i][0] + 1) && ($currY == $arr[$i][1])) {
+            $newGrouping->addGroup($group);
+            $newGrouping->addGroup([$arr[$i][0], $arr[$i][1]]);
+            echo "----in second if condition \n";
+            self::insert($newGrouping->getGrouping(), $newGrouping->getGroupSize());
+          }
+          echo "created new group \n";
         }
-        return $isInGrouping;
+      }
+      return [$isInGrouping, clone $newGrouping];
     }
-    // create a method which receives groupingplayer/ parses,
-    // and returns coordinates
-    public function getCoordX($group)
-    {
-        $xCoord = 0;
-        foreach ($group as $val) {
-            $xCoord = $val[0];
-        }
-        return $xCoord;
-    }
+  }
 
-    public function getCoordY($group)
-    {
-        $yCoord = 0;
-        foreach ($group as $val) {
-            $yCoord = end($val);
-        }
-        return $yCoord;
+  // create a method which receives groupingplayer/ parses,
+  // and returns coordinates
+  public function getCoordX($group)
+  {
+    $xCoord = 0;
+    foreach ($group as $val) {
+      $xCoord = $val[0];
     }
+    return $xCoord;
+  }
 
-    public function printGroupings($groups)
-    {
-        foreach ($groups as $val) {
-            echo "Coords: ";
-            foreach ($val as $coords) {
-                echo $coords . " ";
-            }
-            echo "\n";
-        }
+  public function getCoordY($group)
+  {
+    $yCoord = 0;
+    foreach ($group as $val) {
+      $yCoord = end($val);
     }
+    return $yCoord;
+  }
+
+  public function printGroupings($groups)
+  {
+    foreach ($groups as $val) {
+      echo "Coords: ";
+      foreach ($val as $coords) {
+        echo $coords . " ";
+      }
+      echo "\n";
+    }
+  }
 }
 
 #################################################################
 # TEST CODE
 
-$group1 = [1,1];
-$group2 = [2,2];
-$group3 = [3,3];
+$group1 = [1, 1];
+$group2 = [2, 2];
+$group3 = [3, 3];
 
-$group4 = [1,2];
+$group4 = [1, 2];
 
 $grouping1 = new GroupingPlayer();
 $player1 = new Player();
@@ -189,22 +200,25 @@ $grouping1->addGroup($group1);
 $grouping1->addGroup($group2);
 $player1->insert($grouping1->getGrouping(), $grouping1->getGroupSize());
 
-// commenting this out allows the second if to work
-// idk why
+$inVariable1 = $player1->inGrouping($group3, $grouping1, $player1);
 
-if ($player1->inGrouping($group3, $grouping1, $player1)) {
-    echo "in grouping 1 \n";
+if ($inVariable1[0]) {
+  echo "in grouping 1 \n";
 } else {
-    echo "not in grouping 1 \n";
+  echo "not in grouping 1 \n";
 }
 
-// if u go to line 124 it doesn't enter the if or the else or anything >:((((
-if ($player1->inGrouping($group4, $grouping1, $player1)) {
-    echo "group 4 in grouping 1 \n";
+print_r($inVariable1[1]);
+
+$inVariable2 = $player1->inGrouping($group4, $grouping1, $player1);
+
+if ($inVariable2[0]) {
+  echo "group 4 in grouping 1 \n";
 } else {
-    echo "group 4 not in grouping 1 \n";
+  echo "group 4 not in grouping 1 \n";
 }
 
+print_r($inVariable2[1]);
 
 /*
 // one grouping
--- src/play/SmartStrategy.php
+++ src/play/SmartStrategy.php
@@ -3,106 +3,71 @@
 include "Player.php";
 include "GroupingPlayer.php";
 
-$compStart = True;
+$compStart = true;
 
-class SmartStrategy extends MoveStrategy{
+class SmartStrategy extends MoveStrategy
+{
+    public function computerStart()
+    {
+        global $compStart;
 
-  function computerStart(){
-    global $compStart;
 
+        if ($compStart) {
+            $compStart = false;
 
-    if($compStart){
-      $compStart = False;
-
-      $selectX = 7;
-      $selectY = 7;
+            $selectX = 7;
+            $selectY = 7;
+        }
     }
-  }
 
-  function pickPlace() {
-      $coordinates1 = [rand(0,15), rand(0,15)];
-      $coordinates2 = [rand(0,15), rand(0,15)];
-      $coordinates3 = [rand(0,15), rand(0,15)];
-      //horizontal
-      if($this->isEmpty($coordinates3[0]+1,$coordinates1[1]) ){
-          $this->insert($coordinates3[0]+1,$coordinates1[1],"Horizontal");
-      }
-      if($this->isEmpty($coordinates1[0]-1,$coordinates1[1]) ){
-          $this->insert($coordinates1[0]-1,$coordinates1[1],"Horizontal");
-      }
-      //vertical
-      if($this->isEmpty($coordinates3[0],$coordinates3[1]+1) ){
-          $this->insert($coordinates3[0],$coordinates3[1]+1,"Vertical");
-      }
-      if($this->isEmpty($coordinates1[0],$coordinates1[1]-1) ){
-          $this->insert($coordinates1[0],$coordinates1[1]-1,"Vertical");
-      }
-      //diagonal right
-      if($this->isEmpty($coordinates3[0]+1,$coordinates3[1]+1) ){
-          $this->insert($coordinates3[0]+1,$coordinates3[1]+1,"FDiagonal");
-      }
-      if($this->isEmpty($coordinates1[0]-1,$coordinates1[1]-1) ){
-          $this->insert($coordinates1[0]-1,$coordinates1[1]-1,"FDiagonal");
-      }
-      //diagonal left
-      if($this->isEmpty($coordinates3[0]-1,$coordinates3[1]+1) ){
-          $this->insert($coordinates3[0]-1,$coordinates3[1]+1,"BDiagonal");
-      }
-      if($this->isEmpty($coordinates1[0]-1,$coordinates1[1]-1) ){
-          $this->insert($coordinates1[0]-1,$coordinates1[1]-1,"BDiagonal");
-      }
+    public function pickPlace()
+    {
+        // place must be picked based off of two criteria:
+        // - there is a valid place
+        // - the player has 3 stones in a row (-,|,\,/) (PRIORITY)
 
-    // place must be picked based off of two criteria:
-    // - there is a valid place
-    // - the player has 3 stones in a row (-,|,\,/) (PRIORITY)
+        // comp starts (7,7) -> middle of board
+        $this->computerStart();
+        // check potential player win
+        // if there is a potential player win:
+        //  - cover one side of 3 row
+        // else, we add stone to:
+        //  - rather new open area
+        //  - or new open area next to an existing stone
 
-    // comp starts (7,7) -> middle of board
-    $this->computerStart();
-    // check potential player win
-    // if there is a potential player win:
-    //  - cover one side of 3 row
-    // else, we add stone to:
-    //  - rather new open area
-    //  - or new open area next to an existing stone
-    if($this->horizontal()){
-      // from Player class, check 
-      // cover open side
-    }
-    else{
-      // from stored computer placements
-      //  - select random coordinate and place near to create
-      //  a computer win
-    }
-  }// aqui we
+    }// aqui we
     // place holder
-    function insert( $x,  $y,$orientation): int
+    public function insert($x, $y, $orientation): int
     {
         return 0;
     }
 
-  function isEmpty($x,$y): bool
-  {
-      return true;
-  }
+    public function isEmpty($x, $y): bool
+    {
+        return true;
+    }
 
-  // check if there are 3 in a row
-  function horizontal( ): bool
-  {
+    // check if there are 3 in a row
+    public function horizontal(): bool
+    {
 
-    return true;
-  }
+        return true;
+    }
 
 
-  function  vertical(): bool{
-      return false;
-  }
-  function diagonalRight(): bool{
-      return true;
-  }
-  function diagonalLeft(): bool{
-      return true;
+    public function vertical(): bool
+    {
+        return false;
+    }
+    public function diagonalRight(): bool
+    {
+        return true;
+    }
+    public function diagonalLeft(): bool
+    {
+        return true;
 
-  }
+    }
 
 
 }
@@ -110,16 +75,16 @@ class SmartStrategy extends MoveStrategy{
 $board = new Board();
 $s = new SmartStrategy($board);
 
-$testPlayer = new Player;
+$testPlayer = new Player();
 // make groups like this to test
 // insert 3 groups
-$coordinates1 = [rand(0,15), rand(0,15)];
-$coordinates2 = [rand(0,15), rand(0,15)];
-$coordinates3 = [rand(0,15), rand(0,15)];
+$coordinates1 = [rand(0, 15), rand(0, 15)];
+$coordinates2 = [rand(0, 15), rand(0, 15)];
+$coordinates3 = [rand(0, 15), rand(0, 15)];
 
 
-$testPlayer->insert($coordinates1,1);
-$testPlayer->insert($coordinates2,2);
+$testPlayer->insert($coordinates1, 1);
+$testPlayer->insert($coordinates2, 2);
 print_r($testPlayer->extract());
 
 $s->pickPlace();

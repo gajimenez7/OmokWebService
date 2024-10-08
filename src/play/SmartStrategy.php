738c394340e05d9569de0bf63cd140d48263ac9b
godi-png <eraticsyndicate@gmail.com>
2024-10-07 23:26:28 -0600

    small formatting changes

--- src/play/SmartStrategy.php
+++ src/play/SmartStrategy.php
@@ -7,69 +7,65 @@ $compStart = true;
 
 class SmartStrategy extends MoveStrategy
 {
-    public function computerStart()
-    {
-        global $compStart;
+  public function computerStart()
+  {
+    global $compStart;
 
 
-        if ($compStart) {
-            $compStart = false;
+    if ($compStart) {
+        $compStart = false;
 
-            $selectX = 7;
-            $selectY = 7;
-        }
+        $selectX = 7;
+        $selectY = 7;
     }
+  }
 
-    public function pickPlace()
-    {
-        // place must be picked based off of two criteria:
-        // - there is a valid place
-        // - the player has 3 stones in a row (-,|,\,/) (PRIORITY)
+  public function pickPlace()
+  {
+    // place must be picked based off of two criteria:
+    // - there is a valid place
+    // - the player has 3 stones in a row (-,|,\,/) (PRIORITY)
 
-        // comp starts (7,7) -> middle of board
-        $this->computerStart();
-        // check potential player win
-        // if there is a potential player win:
-        //  - cover one side of 3 row
-        // else, we add stone to:
-        //  - rather new open area
-        //  - or new open area next to an existing stone
+    // comp starts (7,7) -> middle of board
+    $this->computerStart();
+    // check potential player win
+    // if there is a potential player win:
+    //  - cover one side of 3 row
+    // else, we add stone to:
+    //  - rather new open area
+    //  - or new open area next to an existing stone
 
-    }// aqui we
-    // place holder
-    public function insert($x, $y, $orientation): int
-    {
-        return 0;
-    }
+  }// aqui we
+  // place holder
+  public function insert($x, $y, $orientation): int
+  {
+    return 0;
+  }
 
-    public function isEmpty($x, $y): bool
-    {
-        return true;
-    }
+  public function isEmpty($x, $y): bool
+  {
+    return true;
+  }
 
-    // check if there are 3 in a row
-    public function horizontal(): bool
-    {
-
-        return true;
-    }
-
-
-    public function vertical(): bool
-    {
-        return false;
-    }
-    public function diagonalRight(): bool
-    {
-        return true;
-    }
-    public function diagonalLeft(): bool
-    {
-        return true;
-
-    }
+  // check if there are 3 in a row
+  public function horizontal(): bool
+  {
+    return true;
+  }
 
 
+  public function vertical(): bool
+  {
+    return false;
+  }
+  public function diagonalRight(): bool
+  {
+    return true;
+  }
+  public function diagonalLeft(): bool
+  {
+    return true;
+  }
 }
 
 $board = new Board();
